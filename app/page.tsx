8c75993ff52203f055372e0bf5600a818a111a88
hduquinha <henriqueduquis1@gmail.com>
2025-11-21 15:03:39 -0300

    Wrap home with dashboard layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
-export { default, metadata } from "./(dashboard)/page";
+import DashboardLayout from "./(dashboard)/layout";
+import CrmPage from "./(dashboard)/crm/page";
+
+export { metadata } from "./(dashboard)/crm/page";
 
 export const dynamic = "force-dynamic";
+
+type DashboardPageProps = Parameters<typeof CrmPage>[0];
+
+export default function HomePage(props: DashboardPageProps) {
+	return (
+		<DashboardLayout>
+			<CrmPage {...props} />
+		</DashboardLayout>
+	);
+}
